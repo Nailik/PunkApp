d5a7826372ff582f04f85cc7db44e73821752e8d
Nailik <email@kilianeller.de>
2022-08-29 17:39:33 +0200

    disabled itemview click when expanded

--- app/src/main/java/com/example/punkapp/view/ItemView.kt
+++ app/src/main/java/com/example/punkapp/view/ItemView.kt
@@ -6,41 +6,45 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyItemScope
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.example.punkapp.backend.data.Beer
 
 @Composable
 fun LazyItemScope.ItemView(beer: Beer, isExpanded: Boolean, onClick: () -> Unit) {
-    Card(modifier = Modifier
-        .let {
-            return@let if (isExpanded) {
-                it
-                    .fillParentMaxSize()
-                    .padding(bottom = 8.dp)
-            } else {
-                it.fillMaxSize()
+    Card(
+        modifier = Modifier
+            .let {
+                return@let if (isExpanded) {
+                    it.fillParentMaxSize()
+                } else {
+                    it.fillMaxSize()
+                }
             }
-        }
-        .animateContentSize()
-        .padding(top = 8.dp)
-        .padding(horizontal = 8.dp)
-        .clickable(onClick = onClick),
-        elevation = CardDefaults.outlinedCardElevation(defaultElevation = 5.dp)) {
-        CardContent(beer, isExpanded)
+            .animateContentSize()
+            .padding(8.dp)
+            .clickable(enabled = !isExpanded) {
+                onClick()
+            },
+        elevation = CardDefaults.outlinedCardElevation(defaultElevation = 5.dp)
+    ) {
+        CardContent(beer, isExpanded, onClick)
     }
 }
 
 /**
  * The card content contains an image, text and information baout a beer
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CardContent(beer: Beer, isExpanded: Boolean) {
+fun CardContent(beer: Beer, isExpanded: Boolean, onClose: () -> Unit) {
     var bottomNavItem by remember { mutableStateOf(BottomNavItem.Data) }
-
 
     Column(modifier = Modifier.fillMaxSize()) {
 
@@ -52,10 +56,27 @@ fun CardContent(beer: Beer, isExpanded: Boolean) {
             }
         }
 
+        SmallTopAppBar(
+            title = {
+                Text(beer.name ?: "")
+            },
+            scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(),
+            navigationIcon = {
+                if (isExpanded) {
+                    Icon(
+                        Icons.Filled.Close, contentDescription = "", modifier = Modifier
+                            .clip(CircleShape)
+                            .clickable(onClick = onClose)
+                            .padding(8.dp)
+                    )
+                }
+            }
+        )
+
         when (bottomNavItem) {
             BottomNavItem.Data -> DataView(modifier, beer, isExpanded)
-            BottomNavItem.Recipe -> RecipeView(modifier)
-            BottomNavItem.AddPost -> OtherView(modifier)
+            BottomNavItem.Recipe -> RecipeView(modifier, beer)
+            BottomNavItem.AddPost -> IngredientsView(modifier, beer)
         }
         if (isExpanded) {
             BottomNavigationView(bottomNavItem, onSelectPage = {
